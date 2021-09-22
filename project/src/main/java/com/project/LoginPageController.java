61476b2a030daf10153b063b9c3b994c0e4146b5
Jonas English <JonasEnglish0@gmail.com>
2021-09-22 10:41:25 -0500

    The Login Button now shows a hello world message

--- project/src/main/java/com/project/LoginPageController.java
+++ project/src/main/java/com/project/LoginPageController.java
@@ -1,10 +1,14 @@
 package com.project;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 
+
+// The Controller for the Login Page
 public class LoginPageController {
 
     @FXML
@@ -16,4 +20,15 @@ public class LoginPageController {
     @FXML
     private Button Button_Login;
 
+    @FXML
+    private ProgressIndicator ProgressIndicator_Login;
+
+    @FXML
+    private void OnClickButton_Login(Event e){
+        ProgressIndicator_Login.setProgress(1);
+
+        TextField_Username.setText("Hello World!");
+
+        System.out.println("Hello World!");
+    }
 }
