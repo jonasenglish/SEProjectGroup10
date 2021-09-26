78b6091b97c9de1564b0755d36d3025a2c69cc8e
Jonas English <JonasEnglish0@gmail.com>
2021-09-26 18:59:23 -0500

    Added Basic Account Creation with secure passwords

--- project/src/main/java/com/main/controller/LoginPageController.java
+++ project/src/main/java/com/main/controller/LoginPageController.java
@@ -1,6 +1,11 @@
 package com.main.controller;
 
 import com.main.App;
+import com.main.database.DatabaseManager;
+import com.main.objects.Account;
+import com.main.tools.PasswordUtils;
+
+import org.bson.Document;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +20,8 @@ import javafx.stage.Stage;
 
 // The Controller for the Login Page
 public class LoginPageController {
+
+    private boolean err = false;
 
     @FXML
     private PasswordField TextField_Password;
@@ -58,6 +65,36 @@ public class LoginPageController {
     @FXML
     void OnClickButton_Login(ActionEvent event) {
         // Attempt to Login with Given Credentials.
+        err = false;
+
+        String username = TextField_Username.getText();
+        String password = TextField_Password.getText();
+
+        Document accountDocument = DatabaseManager.instance.FindDocumentByUsername(username, DatabaseManager.instance.GetAccountCollection());
+        if(accountDocument == null){
+            Error_AccountWithUsernameNotFound();
+        }
+
+        if(err) return;
+
+        Account account = Account.ToAccount(accountDocument);
+
+        if(!PasswordUtils.verifyUserPassword(password, account.GetPassword(), account.GetSalt())){
+            Error_PasswordIncorrect();
+            return;
+        }
+
+        System.out.println("Welcome " + username + "!");
+    }
+
+    private void Error_AccountWithUsernameNotFound(){
+        err = true;
+        System.err.println("Could not find account with given username!");
+    }
+
+    private void Error_PasswordIncorrect(){
+        err = true;
+        System.err.println("Password Incorrect!");
     }
 
 }
