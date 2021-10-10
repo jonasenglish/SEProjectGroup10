ba269a085dc3f68602887c0ab920a24be628d503
SpinyKeyboard88 <juan.porraz84@gmail.com>
2021-10-10 17:33:13 -0500

    Slight changes on fxml files, made a class
    I attempted to make a controller class for the managerView page but it just wasn't having it.
    I just left the class there and slightly modified the managerView fxml file.

---
+++ project/src/main/java/com/main/controller/ManagerViewController.java
@@ -0,0 +1,14 @@
+package com.main.controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+//Controller for the manager View
+public class ManagerViewController {
+
+	//uhfgudhg idk why this wouldn't work 
+
+}
