2bbb30e275361e5effd48fc7d82102b2378ee723
Jonas English <JonasEnglish0@gmail.com>
2021-10-20 15:25:17 -0500

    Fixing the repo

--- project/src/main/java/com/main/objects/Reservation.java
+++ project/src/main/java/com/main/objects/Reservation.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+// The Reservations object
 public class Reservation {
     
     // The account that made the Reservation
