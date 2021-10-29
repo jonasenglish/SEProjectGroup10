1854dfe3e8ec1b4d8f4084bf27ded709b7d8951e
Jose Hernandez <46972896+jmhern0818@users.noreply.github.com>
2021-10-29 12:00:06 -0500

    Edited Reservation View Controller
    Buttons have ids and OnClick Functions. Reservation Skeleton Controller now existent.

--- project/src/main/java/com/main/controller/ReservationViewController.java
+++ project/src/main/java/com/main/controller/ReservationViewController.java
@@ -1,7 +1,52 @@
 package com.main.controller;
+import com.main.pages.PageManager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ReservationViewController {
 
-	//basic fxml stuff should go here
-	
+    @FXML
+    private Button Button_Checkout;
+
+    @FXML
+    private Button Button_Delete;
+
+    @FXML
+    private Button Button_Home;
+
+    @FXML
+    private Button Button_Modify;
+
+    @FXML
+    private TextField TextField_input;
+
+    @FXML
+    void OnClick_CheckOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClick_Delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnClick_Home(ActionEvent event) {
+		PageManager.SetPage("ManagerView", "Welcome");
+    }
+
+    @FXML
+    void OnClick_Modify(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnInput_ReservationField(ActionEvent event) {
+
+    }
+
 }
+
