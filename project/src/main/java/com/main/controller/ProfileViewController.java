4eff80431637fc3c156c9a92e883bd8ef9d7f325
wrrjackie <jackietran904@gmail.com>
2021-11-14 19:47:47 -0600

    Almost done with profile functionality
    + added the functionality to how the user will change their username and/or email, just need to actually change it through the database.

--- project/src/main/java/com/main/controller/ProfileViewController.java
+++ project/src/main/java/com/main/controller/ProfileViewController.java
@@ -14,8 +14,6 @@ import javafx.scene.control.TextField;
 
 public class ProfileViewController{
 
-    private boolean err = false;
-
     @FXML
     private Label Label_ProfileName;
 
@@ -52,9 +50,8 @@ public class ProfileViewController{
     @FXML
     void OnClick_Logout(ActionEvent event) {
         clearFields();
-        PageManager.SetPage("Login", "Welcome!");
-
         resetErrorDisplay();
+        PageManager.SetPage("Login", "Welcome!");
     }
 
     @FXML
@@ -66,94 +63,92 @@ public class ProfileViewController{
 
     @FXML
     void OnClick_Edit(ActionEvent event) {
-        //DatabaseManager dm = DatabaseManager.instance;
-        //Account account = dm.FindAccountByUsername(App.currentUser.getUsername());
+        resetErrorDisplay();
+        TextField_Username.setVisible(true);
+        TextField_Email.setVisible(true);
+        PasswordField_Password.setVisible(true);
 
         TextField_Username.setText(App.currentUser.getUsername());
         TextField_Email.setText(App.currentUser.getEmail());
-
-        //System.out.println(account.getPassword() + "\nsalt: " + account.getSalt());
     }
 
     @FXML
     void OnClick_Submit(ActionEvent event) {
         DatabaseManager dm = DatabaseManager.instance;
         Account account = dm.FindAccountByUsername(App.currentUser.getUsername());
-        resetErrorDisplay();
 
         String new_username = TextField_Username.getText();
         String new_email = TextField_Email.getText();
+        resetErrorDisplay();
 
-        if(new_username.length() < 3 || new_username.length() > 20)
-                usernameError_IncorrectFormat();
-            
-        if(!new_email.contains("@") || !new_email.contains("."))
-                emailError_IncorrectFormat();
+        if(new_username.length() > 3 && new_username.length() < 20) {
+            if(new_email.contains("@") && new_email.contains(".")) {
+                if(PasswordUtils.verifyUserPassword(PasswordField_Password.getText(), account.getPassword(), account.getSalt()) ) {
+                    if(dm.FindAccountByUsername(new_username) != null) { // check database if username is taken
+                        if(new_username.equals(App.currentUser.getUsername())) { // check if username is taken but not theirs
+                            // username stays the same or new username was entered
+                            account.setUsername(new_username);
 
-        if(err) return;
-
-        if(PasswordUtils.verifyUserPassword(PasswordField_Password.getText(), account.getPassword(), account.getSalt()) ) {
-
-            if(dm.FindAccountByUsername(new_username) != null) { // check database if username is taken
-                if(new_username.equals(App.currentUser.getUsername())) { // check if username is taken but not theirs
-                    // username stays the same, update email info
+                            System.out.println("Account info updated!\n" + new_username);
+                            System.out.println(new_email);
+                            clearFields();
+                        } 
+                        else { //error username is taken, not theirs either
+                            usernameError_AlreadyExists();
+                            return;
+                        }
+                    }
                     
-                } 
-                else { //error username is taken, not theirs either
-                    usernameError_AlreadyExists();
-                    return;
-                }
-            }
+                    if(dm.FindAccountByEmail(new_email) != null) { // check database if email is taken
+                        if(new_email.equals(App.currentUser.getEmail())) { // check if email is taken but not theirs
+                            // email stays the same or new email was entered
+                            account.setEmail(new_email);
 
-            if(dm.FindAccountByEmail(new_email) != null) { // check database if email is taken
-                if(new_email.equals(App.currentUser.getEmail())) { // check if email is taken but not theirs
-                    // email stays the same, update username
-
+                            System.out.println("Account info updated!\n" + new_username);
+                            System.out.println(new_email);
+                            clearFields();
+                        }
+                        else {
+                            emailError_AlreadyExists();
+                            return;
+                        }
+                    }
                 }
                 else {
-                    emailError_AlreadyExists();
-                    return;
+                    passwordError_Incorrect();
+                    PasswordField_Password.clear();
                 }
             }
-
-            System.out.println("Account info updated!\n" + new_username);
-            System.out.println(new_email);
-
-            clearFields();
+            else {
+                emailError_IncorrectFormat();
+            }
         }
         else {
-            passwordError_Incorrect();
-            PasswordField_Password.clear();
-            return;
+            usernameError_IncorrectFormat();
         }
     }
-
+    
     private void usernameError_AlreadyExists() {
-        err = true;
         Label_UsernameError.setVisible(true);
         Label_UsernameError.setText("This username has already been taken!");
     }
 
     private void usernameError_IncorrectFormat() {
-        err = true;
         Label_UsernameError.setVisible(true);
         Label_UsernameError.setText("Usernames must be between 3 and 20 characters.");
     }
 
     private void emailError_AlreadyExists() {
-        err = true;
         Label_EmailError.setVisible(true);
         Label_EmailError.setText("This email already belongs to another account!");
     }
 
     private void emailError_IncorrectFormat() {
-        err = true;
         Label_EmailError.setVisible(true);
         Label_EmailError.setText("This is not a valid email!");
     }
 
     private void passwordError_Incorrect() {
-        err = true;
         Label_PasswordError.setVisible(true);
         Label_PasswordError.setText("Incorrect Password");
     }
