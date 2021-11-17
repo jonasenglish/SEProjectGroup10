f32f070ad2fadde783f5b91733822901005510c6
Jonas English <JonasEnglish0@gmail.com>
2021-11-17 11:29:52 -0600

    Fixed Non-Loading Issues

--- project/src/main/java/com/main/controller/ManagerViewController.java
+++ project/src/main/java/com/main/controller/ManagerViewController.java
@@ -20,13 +20,14 @@ public class ManagerViewController {
   @FXML
   private Label WelcomeLabel;
 
-/*
+
   @FXML
   public void Initialize(ActionEvent onContextMenuRequested) {
-    WelcomeLabel.setText("Welcome " + App.currentUser.getUsername() + "!");
-    WelcomeLabel.setVisible(true);
     //WelcomeLabel.setText("Welcome " + App.currentUser.getUsername() + "!");
-  }*/
+    //WelcomeLabel.setVisible(true);
+    //WelcomeLabel.setText("Welcome " + App.currentUser.getUsername() + "!");
+  }
+  
 
 	@FXML
   void OnClick_EditHotel(ActionEvent event) {
